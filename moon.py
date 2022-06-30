a4ae999eafeeda3bd4e454e10923e6e687f65e64
melih00can10 <92529973+melih00can10@users.noreply.github.com>
2022-06-30 03:04:12 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -53,7 +53,7 @@ def bul(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"🎶 𝐈̇𝐍𝐃𝐈̇𝐑𝐈̇𝐋𝐃𝐈̇ 🎶."
+        rep = f"🎶 𝐈̇𝐍𝐃𝐈̇𝐑𝐈̇𝐋𝐃𝐈̇ 🎶\n\n**İndirdiğiniz Tüm Müzikler Ve Daha Fazlası @YoutubeVcMuzik Kanalımızda."
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
