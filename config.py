6bab8e637d70443b6e789ef28dbf9422f317bf6e
melih00can10 <92529973+melih00can10@users.noreply.github.com>
2022-07-14 00:17:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_OWNER = os.environ.get("BOT_OWNER")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME")
-    GROUP = os.environ.get("GROUP")
-    DURATION_LIMIT = int(os.environ.get("DURATION_LIMIT", "7"))
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID"))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5446861802:AAFmJ5pYhzjmsqTDXLLn1axv6MbZraA8lxk")
+    API_ID = int(os.environ.get("API_ID", "19785832"))
+    API_HASH = os.environ.get("API_HASH", "5a894d5786057b269caf4ae788d5ecc2")
+    BOT_OWNER = os.environ.get("BOT_OWNER", "DeepKraL")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "youtubeVcproBot")
+    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "youtubevcmuzik")
+    GROUP = os.environ.get("GROUP", "SohbetMiss") 
+    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001259764610"))
