6d5e315798f14d4698083540620028ed29316b13
melih00can10 <92529973+melih00can10@users.noreply.github.com>
2022-07-26 02:25:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5446861802:AAFmJ5pYhzjmsqTDXLLn1axv6MbZraA8lxk")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5446861802:AAEUkd9vTiKZpF1QJp1HdWA_c2xnY0vhqVw")
     API_ID = int(os.environ.get("API_ID", "19785832"))
     API_HASH = os.environ.get("API_HASH", "5a894d5786057b269caf4ae788d5ecc2")
     BOT_OWNER = os.environ.get("BOT_OWNER", "DeepKraL")
