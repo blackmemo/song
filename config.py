1769b00a149b4187dafa8138ee0f72c4b55f31d8
melih00can10 <92529973+melih00can10@users.noreply.github.com>
2022-08-13 19:15:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5446861802:AAEUkd9vTiKZpF1QJp1HdWA_c2xnY0vhqVw")
     API_ID = int(os.environ.get("API_ID", "19785832"))
     API_HASH = os.environ.get("API_HASH", "5a894d5786057b269caf4ae788d5ecc2")
-    BOT_OWNER = os.environ.get("BOT_OWNER", "DeepKraL")
+    BOT_OWNER = os.environ.get("BOT_OWNER", "Youtube")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "youtubeVcproBot")
     PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "youtubevcmuzik")
     GROUP = os.environ.get("GROUP", "SohbetMiss") 
