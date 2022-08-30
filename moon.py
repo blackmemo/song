1ea65fcbff1bdf5247460fe5e13e0be3f3a35644
melih022 <110200865+melih022@users.noreply.github.com>
2022-08-30 14:28:35 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -170,7 +170,7 @@ def bul(_, message):
             secmul *= 60
         m.edit("•> **Yükleniyor**...")
         
-        message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name, performer="@YouTubeVcProBot"),
+        message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, reply_markup=keyboard, thumb=thumb_name, performer="@YouTubeVcProBot"),
         keyboard = InlineKeyboardMarkup(
            [
                 [
@@ -182,7 +182,7 @@ def bul(_, message):
             ]
         )
         m.delete()
-        bot.send_audio(chat_id=Config.PLAYLIST_ID, audio=audio_file, caption=mel, performer="@YouTubeVcProBot", parse_mode='md', title=title, duration=dur, thumb=thumb_name),
+        bot.send_audio(chat_id=Config.PLAYLIST_ID, audio=audio_file, caption=mel, performer="@YouTubeVcProBot", parse_mode='md', title=title, duration=dur, thumb=thumb_name,reply_markup=keyboard),
         keyboard = InlineKeyboardMarkup(
            [
                 [
