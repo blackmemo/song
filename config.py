a9595a01a079e1d85cc55654d69e89028e04b8a7
melih022 <110200865+melih022@users.noreply.github.com>
2022-09-11 22:57:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5446861802:AAH4E_alivTM3S18jE3jPAE8LMGMiHHGW_k")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5446861802:AAHj34aD5vzpMbNCRT1ZoKoxbr_VZn30-0s")
     API_ID = int(os.environ.get("API_ID", "19785832"))
     API_HASH = os.environ.get("API_HASH", "5a894d5786057b269caf4ae788d5ecc2")
     BOT_OWNER = os.environ.get("BOT_OWNER", "Youtube")
